6c81a43f1e310894ba793ce47db2d297fc39cc82
mae616 <oya2.mi@hotmail.co.jp>
2024-12-10 09:08:10 +0900

    move left or right

--- index.js
+++ index.js
@@ -5,9 +5,16 @@ keypress(process.stdin);
 process.stdin.setRawMode(true);
 process.stdin.resume();
 
+// ぷよの色たち
 const color = ["red", "green", "blue", "yellow", "cyan", "magenta", "white"];
+// フィールドの大きさ
 const HEIGHT = 10;
 const WIDTH = 5;
+// enum key移動
+const MOVE = Object.freeze({
+  LEFT: {x: -1, y: 0, check: (x) => x > 0},
+  RIGHT: {x: 1, y: 0, check: (x) => x < WIDTH - 1},
+});
 
 clivas.cursor(false);
 clivas.pin(HEIGHT);
@@ -15,6 +22,24 @@ clivas.pin(HEIGHT);
 const randomColor = () => color[Math.floor(Math.random() * color.length)];
 
 let fields = new Array(WIDTH).fill("").map(() => new Array(HEIGHT).fill("."));
+
+// 移動処理
+const keypressMove = (move) => {
+  const isMoved = new Set();
+  for (let column = WIDTH - 1; column >= 0; column--) {
+    for (let row = HEIGHT - 1; row >= 0; row--) {
+      if (fields[column][row].startsWith("ACT:")) {
+        if (move.check(column)
+          && !isMoved.has(`${column}:${row}`)
+          && fields[column + move.x][row] === ".") {
+          fields[column + move.x][row] = fields[column][row];
+          fields[column][row] = ".";
+          isMoved.add(`${column + move.x}:${row}`);
+        }
+      }
+    }
+  }
+};
 
 // 落ちる処理
 const fall = (fields) => {
@@ -46,18 +71,25 @@ const fall = (fields) => {
   }
   return fields;
 };
-const draw = () => {
-  // すでに落ちているものがない場合
-  if (!fields.some((column) => column.some((row) => row.startsWith("ACT:")))) {
-    // 落とす処理
-    fields[2][0] = `ACT:${randomColor()}`;
-    fields[2][1] = `ACT:${randomColor()}`;
-  } else {
-    // 落ちる処理
-    fields = fall(fields);
+
+const draw = (isFall) => {
+  clivas.clear();
+
+  if (isFall) {
+    // すでに落ちているものがない場合
+    if (
+      !fields.some((column) => column.some((row) => row.startsWith("ACT:")))
+    ) {
+      // 落とす処理
+      fields[2][0] = `ACT:${randomColor()}`;
+      fields[2][1] = `ACT:${randomColor()}`;
+    } else {
+      // 落ちる処理
+      fields = fall(fields);
+    }
   }
 
-  // 行と列を入れ替える処理
+  // 行と列を入れ替える
   const fields2 = new Array(HEIGHT)
     .fill("")
     .map(() => new Array(WIDTH).fill("."));
@@ -67,7 +99,7 @@ const draw = () => {
     }
   }
 
-  clivas.clear();
+  // 実描画
   clivas.line("{white:┌──────────┐}");
   for (const row of fields2) {
     clivas.write("{white:│}");
@@ -87,7 +119,7 @@ const draw = () => {
 
 let flame = 0;
 setInterval(function () {
-  draw();
+  draw(true);
   flame++;
 }, 800);
 
@@ -100,9 +132,11 @@ process.stdin.on("keypress", (ch, key) => {
   }
   if (key.name === "left") {
     // move left
+    keypressMove(MOVE.LEFT);
   }
   if (key.name === "right") {
     // move right
+    keypressMove(MOVE.RIGHT);
   }
   if (key.name === "down") {
     // move down
@@ -110,5 +144,5 @@ process.stdin.on("keypress", (ch, key) => {
   if (key.name === "up") {
     // rotate
   }
-  draw();
+  draw(false);
 });
